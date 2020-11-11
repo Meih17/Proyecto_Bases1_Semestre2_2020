aab3d224c41120968d376683a428fbb5b5885160
= <=>
2020-11-11 12:29:24 -0600

    PB Schema creation

--- 1. Tablespace_and_Schema.sql
+++ 1. Tablespace_and_Schema.sql
@@ -15,3 +15,17 @@ CREATE TABLESPACE PB_Ind
     AUTOEXTEND ON
     NEXT 512k
     MAXSIZE 200M;
+
+--PB Schema
+CREATE USER PB
+    IDENTIFIED BY PB
+    DEFAULT TABLESPACE pb_data
+    TEMPORARY TABLESPACE temp
+    QUOTA 5M ON system;
+    --PROFILE esteb
+    --PASSWORD EXPIRE;
+
+GRANT connect TO PB;
+GRANT create session to PB;
+GRANT create table to PB;
+grant create view to PB;
