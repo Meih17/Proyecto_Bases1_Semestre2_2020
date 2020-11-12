6b67e5ef02c0e32ecff8934602ff22059cce6965
SmokeCR <estebanvquiros@gmail.com>
2020-11-12 02:15:35 -0600

    Emailxperson table creation and some bugs fixes

--- 2. Database_tables_and_constraints.sql
+++ 2. Database_tables_and_constraints.sql
@@ -1,16 +1,38 @@
+
+--Table to store the phone number of people
 CREATE TABLE PB.phone
 (
   PhoneNumberID NUMBER(10) CONSTRAINT phone_phonenumberid_nn NOT NULL, CONSTRAINT pk_phone PRIMARY KEY(PhoneNumberID),
   PhomeNumber NUMBER(10) CONSTRAINT Phone_phonenumber_nn NOT NULL,
 );
 
+-- n to n relation table of phone and person
 CREATE TABLE PB.phonexperson
 (
-  PhoneNumberID NUMBER(10) CONSTRAINT phonexperson_phonenumberid_nn NOT NULL,
-  idnumber NUMBER(15) CONSTRAINT phonexperson_idnumber_nn NOT NULL,
+  PhoneNumberID NUMBER(10),
+  idnumber NUMBER(15),
   CONSTRAINT pk_phonexperson PRIMARY KEY (PhoneNumberID, idnumber),
-  CONSTRAINT fk_personxphoto FOREIGN KEY (PhoneNumberID) REFERENCES PB.Phone(PhoneNumberID, idnumber)
+  CONSTRAINT fk_personxphoto_nn FOREIGN KEY (PhoneNumberID) REFERENCES PB.Phone(PhoneNumberID),
+  CONSTRAINT fk_personxphoto_nm FOREIGN KEY (idnumber) REFERENCES PB.Person(idnumber)
 );
+
+--Table to store the emails addresses
+CREATE TABLE PB.email
+(
+  IDEmail NUMBER(10) CONSTRAINT email_idemail_nn NOT NULL, CONSTRAINT pk_email PRIMARY KEY (IDEmail),
+  EmailAddress VARCHAR2(25) email_emailaddress_nn NOT NULL
+);
+
+CREATE TABLE PB.emailxperson
+(
+  IDEmail NUMBER(10),
+  idnumber NUMBER(15),
+  CONSTRAINT pk_emailxperson PRIMARY KEY (IDEmail, idnumber),
+  CONSTRAINT fk_emailxperson_nn FOREIGN KEY (IDEmail) REFERENCES PB.email(IDEmail),
+  CONSTRAINT fk_emailxperson_nm FOREIGN KEY (idnumber) REFERENCES PB.person(idnumber)
+);
+
+
 
 CREATE TABLE PB.person
 (
@@ -21,5 +43,5 @@ CREATE TABLE PB.person
   Nationality VARCHAR2(20) CONSTRAINT person_nationality_nn NOT NULL,
   PersonPhotoID NUMBER(15) CONSTRAINT fk_person_photo FOREIGN KEY (PersonPhotoID) REFERENCES PB.Photo(PersonPhotoID),
   IDType NUMBER(5) CONSTRAINT fk_person_typeofid FOREIGN KEY (IDType) REFERENCES PB.TypeOfID(IDType),
-  AddressID NUMBER(15) CONSTRAINT fk_person_address FOREIGN KEY (AddressID) REFERENCIES PB.Address(AddressID)
+  AddressID NUMBER(15) CONSTRAINT fk_person_address FOREIGN KEY (AddressID) REFERENCES PB.Address(AddressID)
 );
