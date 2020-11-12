e0d74be589358e4c76b83dde6aef6d52f67e5ff9
SmokeCR <estebanvquiros@gmail.com>
2020-11-12 01:36:23 -0600

    Some fixes

--- 2. Database_tables_and_constraints.sql
+++ 2. Database_tables_and_constraints.sql
@@ -7,8 +7,9 @@ CREATE TABLE PB.phone
 CREATE TABLE PB.phonexperson
 (
   PhoneNumberID NUMBER(10) CONSTRAINT phonexperson_phonenumberid_nn NOT NULL,
-  idnumber NUMBER(15) CONSTRAINT phonexperson_idnumber_nn NOT NULL, CONSTRAINT pk_phonexperson PRIMARY KEY (PhoneNumberID, idnumber),
-  CONSTRAINT fk_personxphoto FOREIGN KEY (PhoneNumberID) REFERENCES PB.Phone(PhoneNumberID, idnumber) 
+  idnumber NUMBER(15) CONSTRAINT phonexperson_idnumber_nn NOT NULL,
+  CONSTRAINT pk_phonexperson PRIMARY KEY (PhoneNumberID, idnumber),
+  CONSTRAINT fk_personxphoto FOREIGN KEY (PhoneNumberID) REFERENCES PB.Phone(PhoneNumberID, idnumber)
 );
 
 CREATE TABLE PB.person
@@ -18,7 +19,7 @@ CREATE TABLE PB.person
   DateOfBirth VARCHAR2(15) CONSTRAINT person_dateofbirth_nn NOT NULL,
   AGE NUMBER(2) CONSTRAINT person_age_nn NOT NULL,
   Nationality VARCHAR2(20) CONSTRAINT person_nationality_nn NOT NULL,
-  PersonPhotoID NUMBER(15), CONSTRAINT fk_person_photo FOREIGN KEY (PersonPhotoID) REFERENCES PB.Photo(PersonPhotoID),
-  IDType NUMBER(5), CONSTRAINT fk_person_typeofid FOREIGN KEY (IDType) REFERENCES PB.TypeOfID(IDType),
-  AddressID NUMBER(15), fk_person_address FOREIGN KEY (AddressID) REFERENCIES PB.Address(AddressID)
+  PersonPhotoID NUMBER(15) CONSTRAINT fk_person_photo FOREIGN KEY (PersonPhotoID) REFERENCES PB.Photo(PersonPhotoID),
+  IDType NUMBER(5) CONSTRAINT fk_person_typeofid FOREIGN KEY (IDType) REFERENCES PB.TypeOfID(IDType),
+  AddressID NUMBER(15) CONSTRAINT fk_person_address FOREIGN KEY (AddressID) REFERENCIES PB.Address(AddressID)
 );
