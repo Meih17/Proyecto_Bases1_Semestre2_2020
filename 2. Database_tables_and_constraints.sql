d8b14de695d4a196b8cca39ebc56027d5d187b04
= <=>
2020-11-11 20:44:20 -0600

    Foreign and Primary Keys

--- 2. Database_tables_and_constraints.sql
+++ 2. Database_tables_and_constraints.sql
@@ -1,11 +1,11 @@
 CREATE TABLE PB.person
 (
-  idnumber NUMBER(15) CONSTRAINT person_idnumber_nn NOT NULL,
+  idnumber NUMBER(15) CONSTRAINT person_idnumber_nn NOT NULL, CONSTRAINT pk_person PRIMARY KEY (idnumber),
   Name VARCHAR2(20) CONSTRAINT person_name_nn NOT NULL,
   DateOfBirth VARCHAR2(15) CONSTRAINT person_dateofbirth_nn NOT NULL,
   AGE NUMBER(2) CONSTRAINT person_age_nn NOT NULL,
   Nationality VARCHAR2(20) CONSTRAINT person_nationality_nn NOT NULL,
-  PersonPhotoID NUMBER(15),
-  IDType NUMBER(5),
-  AddressID NUMBER(15),
+  PersonPhotoID NUMBER(15), CONSTRAINT fk_person_photo FOREIGN KEY (PersonPhotoID) REFERENCES PB.Photo(PersonPhotoID),
+  IDType NUMBER(5), CONSTRAINT fk_person_typeofid FOREIGN KEY (IDType) REFERENCES PB.TypeOfID(IDType),
+  AddressID NUMBER(15), fk_person_address FOREIGN KEY (AddressID) REFERENCIES PB.Address(AddressID)
 );
